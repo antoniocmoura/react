e66fb633df47daee75eed873a4af2e2cc6f60afd
Antonio Carlos de Moura <antoniocmoura@gmail.com>
2022-04-10 12:32:09 -0300

    exercício react

--- src/index.js
+++ src/index.js
@@ -2,14 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/App';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import About from './pages/About';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
     <Routes>
       <Route path="/" element={<App />} />
-      <Route path="about" element={<About />} />
     </Routes>
   </BrowserRouter>,
 );
